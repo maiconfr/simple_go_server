f5071a29756bda1ccc36ee09fb8fbf13ab19134e
maiconfr <maicon.fr95@gmail.com>
2023-04-18 15:54:35 +0200

    add formulario em uma web page enviando infos via POST

--- server.go
+++ server.go
@@ -20,8 +20,25 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello teste")
 }
 
+func formHandler(w http.ResponseWriter, r *http.Request) {
+	if err := r.ParseForm(); err != nil {
+		fmt.Fprintf(w, "ParseForm() err: %v", err)
+		return
+	}
+	fmt.Fprintf(w, "POST request successful\n")
+	name := r.FormValue("name")
+	address := r.FormValue("address")
+
+	fmt.Fprintf(w, "Name = %s\n", name)
+	fmt.Fprintf(w, "Address = %s\n", address)
+}
+
 func main() {
+	fileServer := http.FileServer((http.Dir("./static"))) //retorna o conteúdo do index.html da pasta 'static'
+	http.Handle("/", fileServer)
 	http.HandleFunc("/hello", helloHandler)
+
+	http.HandleFunc("/form", formHandler)
 
 	fmt.Printf("starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
