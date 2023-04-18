6869334e2f2b14e11642768ad644e3778b34b882
maiconfr <maicon.fr95@gmail.com>
2023-04-18 15:02:23 +0200

    add rotinas basicas de seguranca

--- server.go
+++ server.go
@@ -6,10 +6,22 @@ import (
 	"net/http"
 )
 
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/hello" {
+		http.Error(w, "404 not found.", http.StatusNotFound)
+		return
+	}
+
+	if r.Method != "GET" {
+		http.Error(w, "Method is not supported", http.StatusNotFound)
+		return
+	}
+
+	fmt.Fprintf(w, "Hello teste")
+}
+
 func main() {
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello 2")
-	})
+	http.HandleFunc("/hello", helloHandler)
 
 	fmt.Printf("starting server at port 8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
